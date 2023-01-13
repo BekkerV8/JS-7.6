68044c6a981e26669ace1314b4704ea0d1664e34
BekkerV8 <valkenshtein@gmail.com>
2023-01-12 16:00:38 -0800

    correction tests and config.js

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -25,19 +25,37 @@ describe("When user is on login page, user", () => {
   it("Should add book", () => {
     cy.visit("localhost:3000");
     cy.login("bropet@mail.ru", "123");
-    cy.contains("Добро пожаловать");
-    cy.createBook("War and Peace", "Tolstoy");
-    cy.createBook("Big Daddy", "Korolev");
-    cy.createBook("War", "Suvorov");
-    cy.get(
-      '[href="book/771bff3c-205e-4a74-92ba-b53cd2145603"] > .h-100 > .card-footer > .btn'
-    ).click();
-    cy.get(
-      '[href="book/93ee9675-ef6f-47c5-b40a-cbd92b062aab"] > .h-100 > .card-footer > .btn'
-    ).click();
+    cy.contains("Add new").click();
+    cy.get("#title").type("War and Peace");
+    cy.get("#description").type("Роман");
+    cy.get("#authors").type("Tolstoy");
+    cy.get("form > .ml-2").click();
+    cy.contains("Tolstoy").should("be.visible");
+  });
+  it("add to favorite", () => {
+    cy.visit("localhost:3000");
+    cy.login("bropet@mail.ru", "123");
+    cy.contains("Add new").click();
+    cy.get("#title").type("War and Peace");
+    cy.get("#description").type("Роман");
+    cy.get("#authors").type("Tolstoy");
+    cy.get("#favorite").click();
+    cy.get("form > .ml-2").click();
     cy.get("h4").click();
-    cy.contains("War and Peace");
-    cy.contains("Big Daddy");
+    cy.contains("Tolstoy").should("be.visible");
+    // cy.contains("Добро пожаловать");
+    // cy.createBook("War and Peace", "Tolstoy");
+    // cy.createBook("Big Daddy", "Korolev");
+    // cy.createBook("War", "Suvorov");
+    // cy.get(
+    //   '[href="book/771bff3c-205e-4a74-92ba-b53cd2145603"] > .h-100 > .card-footer > .btn'
+    // ).click();
+    // cy.get(
+    //   '[href="book/93ee9675-ef6f-47c5-b40a-cbd92b062aab"] > .h-100 > .card-footer > .btn'
+    // ).click();
+    // cy.get("h4").click();
+    // cy.contains("War and Peace");
+    // cy.contains("Big Daddy");
   });
 
   //удаление книги из избранного
@@ -47,12 +65,10 @@ describe("When user is on login page, user", () => {
     cy.contains("Добро пожаловать");
     cy.get("h4").click();
     cy.get(
-      '[href="book/93ee9675-ef6f-47c5-b40a-cbd92b062aab"] > .h-100 > .card-footer > .btn'
+      "#root > div > div > a:nth-child(1) > div > div.card-footer"
     ).click();
-    cy.get(
-      '[href="book/771bff3c-205e-4a74-92ba-b53cd2145603"] > .h-100 > .card-footer > .btn'
-    ).click();
-    cy.get(".d-flex > .btn").click();
-    cy.contains("Добро пожаловать");
+    cy.contains("Please add some book to favorit on home page!").should(
+      "be.visible"
+    );
   });
 });
